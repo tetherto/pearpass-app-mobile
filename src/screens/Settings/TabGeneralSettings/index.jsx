a212f3d894d35f7366aa5b34cd929720c8e341a4
Nikoloz Dolidze <n.dolidze@noxtton.com>
2026-01-26 15:02:20 +0400

    Reset biometrics after password change (#100)
    * Reset biometrics after password change
    
    * Add missing JSDocs

--- src/screens/Settings/TabGeneralSettings/index.jsx
+++ src/screens/Settings/TabGeneralSettings/index.jsx
@@ -11,25 +11,22 @@ import Toast from 'react-native-toast-message'
 
 import { DeviceSection } from './DeviceSection'
 import { LanguageSection } from './LanguageSection'
+import { MasterPasswordSection } from './MasterPasswordSection'
 import { ReportSection } from './ReportSection'
-import { ManageVaultsContainer, Version } from './styles'
+import { Version } from './styles'
 import { version } from '../../../../package.json'
 import { CardSingleSetting } from '../../../components/CardSingleSetting'
-import { ListItem } from '../../../components/ListItem'
 import {
   GOOGLE_FORM_KEY,
   GOOGLE_FORM_MAPPING,
   SLACK_WEBHOOK_URL_PATH
 } from '../../../constants/feedback'
-import { ModifyMasterVaultModalContent } from '../../../containers/Modal/ModifyMasterVaultModalContent'
-import { useModal } from '../../../context/ModalContext'
 import { useLanguageOptions } from '../../../hooks/useLanguageOptions'
 import { logger } from '../../../utils/logger'
 
 export const TabGeneralSettings = () => {
   const { t } = useLingui()
   const [language, setLanguage] = useState(i18n.locale)
-  const { openModal } = useModal()
   const handleChangeLanguage = (newLang) => {
     setLanguage(newLang)
     i18n.activate(newLang)
@@ -91,9 +88,6 @@ export const TabGeneralSettings = () => {
       })
     }
   }
-  const handleMasterEditClick = () => {
-    openModal(<ModifyMasterVaultModalContent />)
-  }
 
   return (
     <>
@@ -104,14 +98,7 @@ export const TabGeneralSettings = () => {
         languageOptions={languageOptions}
       />
 
-      <CardSingleSetting title={t`Passwords`}>
-        <ManageVaultsContainer>
-          <ListItem
-            name={t`Master Vault`}
-            onEditClick={handleMasterEditClick}
-          />
-        </ManageVaultsContainer>
-      </CardSingleSetting>
+      <MasterPasswordSection />
 
       <ReportSection
         message={message}
