890312dba751e72446239ad1a286b4e0f0ea031a
Harri Saar Noxtton <h.saar@noxtton.com>
2026-02-17 12:24:06 +0400

    init ts support files (#169)

--- src/libComponents/ButtonPrimary/styles.ts
+++ src/libComponents/ButtonPrimary/styles.ts
@@ -1,6 +1,17 @@
 import styled from 'styled-components/native'
+import { TouchableOpacity, Text } from 'react-native'
 
-export const Button = styled.TouchableOpacity`
+interface ButtonProps {
+  size: 'sm' | 'md'
+  stretch?: boolean
+  disabled?: boolean
+}
+
+interface ButtonTextProps {
+  size: 'sm' | 'md'
+}
+
+export const Button = styled(TouchableOpacity) <ButtonProps>`
   display: inline-flex;
   padding: ${({ size }) => (size === 'sm' ? '10px 15px' : '10px 40px')};
   align-self: ${({ stretch }) => (stretch ? 'stretch' : 'flex-start')};
@@ -15,7 +26,7 @@ export const Button = styled.TouchableOpacity`
   opacity: ${({ disabled }) => (disabled ? 0.6 : 1)};
 `
 
-export const ButtonText = styled.Text`
+export const ButtonText = styled(Text) <ButtonTextProps>`
   color: ${({ theme }) => theme.colors.black.mode1};
   font-family: 'Inter';
   font-size: ${({ size }) => (size === 'sm' ? '12px' : '16px')};
