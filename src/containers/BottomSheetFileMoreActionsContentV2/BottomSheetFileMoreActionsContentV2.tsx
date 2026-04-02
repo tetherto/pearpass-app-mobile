e6692bf7be8570ba473b438865b5f2b806f19894
ndolidze <n.dolidze@noxtton.com>
2026-04-02 16:48:47 +0400

    Add delete confirmation step

--- src/containers/BottomSheetFileMoreActionsContentV2/BottomSheetFileMoreActionsContentV2.tsx
+++ src/containers/BottomSheetFileMoreActionsContentV2/BottomSheetFileMoreActionsContentV2.tsx
@@ -14,7 +14,7 @@ import {
 } from '@tetherto/pearpass-lib-ui-kit/icons'
 import { View } from 'react-native'
 import { styles } from './BottomSheetFileMoreActionsContentV2Styles'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useBottomSheetModal } from '@gorhom/bottom-sheet'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { useLingui } from '@lingui/react/macro'
@@ -33,7 +33,9 @@ export const BottomSheetFileMoreActionsContentV2 = ({
   filename
 }: BottomSheetFileMoreActionsContentV2Props) => {
   const [name, setName] = useState(filename)
-  const [isRenaming, setIsRenaming] = useState(false)
+  const [state, setState] = useState<'default' | 'renaming' | 'deleting'>(
+    'default'
+  )
   const { t } = useLingui()
 
   const isImage = imageRegex.test(filename)
@@ -41,6 +43,12 @@ export const BottomSheetFileMoreActionsContentV2 = ({
   const { theme } = useTheme()
   const { dismiss } = useBottomSheetModal()
   const { bottom } = useSafeAreaInsets()
+
+  useEffect(() => {
+    return () => {
+      setState('default')
+    }
+  }, [])
 
   return (
     <ContextMenu
@@ -53,7 +61,75 @@ export const BottomSheetFileMoreActionsContentV2 = ({
         />
       }
     >
-      {isRenaming ? (
+      {state === 'default' && (
+        <View style={{ paddingBottom: bottom }}>
+          <NavbarListItem
+            label="Rename"
+            onClick={() => setState('renaming')}
+            icon={<Edit color={theme.colors.colorTextPrimary} />}
+            showDivider
+          />
+          <NavbarListItem
+            label="Delete"
+            variant="destructive"
+            onClick={() => {
+              setState('deleting')
+            }}
+            icon={
+              <TrashOutlined
+                color={theme.colors.colorSurfaceDestructiveElevated}
+              />
+            }
+            showDivider
+          />
+        </View>
+      )}
+
+      {state === 'deleting' && (
+        <View style={[styles.container, { paddingBottom: bottom }]}>
+          <View style={styles.header}>
+            <View style={styles.headerTitle}>
+              <Text variant="bodyEmphasized">
+                {isImage ? t`Delete Image` : t`Delete File`}
+              </Text>
+            </View>
+            <Button
+              variant="tertiary"
+              size="medium"
+              aria-label="More options"
+              iconBefore={<Close color={theme.colors.colorTextPrimary} />}
+              onClick={() => setState('default')}
+            />
+          </View>
+          <View style={styles.content}>
+            <Text variant="bodyEmphasized">
+              {isImage
+                ? t`Are you sure you want to delete this image?`
+                : t`Are you sure you want to delete this file?`}
+            </Text>
+            <Button
+              variant="destructive"
+              size="medium"
+              onClick={() => {
+                handleDelete()
+                setState('default')
+                dismiss()
+              }}
+            >
+              {t`Delete`}
+            </Button>
+            <Button
+              variant="secondary"
+              size="medium"
+              onClick={() => setState('default')}
+            >
+              {t`Discard`}
+            </Button>
+          </View>
+        </View>
+      )}
+
+      {state === 'renaming' && (
         <View style={[styles.container, { paddingBottom: bottom }]}>
           <View style={styles.header}>
             <View style={styles.headerTitle}>
@@ -66,7 +142,7 @@ export const BottomSheetFileMoreActionsContentV2 = ({
               size="medium"
               aria-label="More options"
               iconBefore={<Close color={theme.colors.colorTextPrimary} />}
-              onClick={() => setIsRenaming(false)}
+              onClick={() => setState('default')}
             />
           </View>
           <View style={styles.content}>
@@ -80,7 +156,7 @@ export const BottomSheetFileMoreActionsContentV2 = ({
               size="medium"
               onClick={() => {
                 handleRename(name || '')
-                setIsRenaming(false)
+                setState('default')
                 dismiss()
               }}
             >
@@ -89,35 +165,11 @@ export const BottomSheetFileMoreActionsContentV2 = ({
             <Button
               variant="secondary"
               size="medium"
-              onClick={() => setIsRenaming(false)}
+              onClick={() => setState('default')}
             >
               {t`Discard`}
             </Button>
           </View>
-        </View>
-      ) : (
-        <View style={{ paddingBottom: bottom }}>
-          <NavbarListItem
-            label="Rename"
-            onClick={() => setIsRenaming(true)}
-            icon={<Edit color={theme.colors.colorTextPrimary} />}
-            showDivider
-          />
-          <NavbarListItem
-            label="Delete"
-            variant="destructive"
-            onClick={() => {
-              handleDelete()
-              setIsRenaming(false)
-              dismiss()
-            }}
-            icon={
-              <TrashOutlined
-                color={theme.colors.colorSurfaceDestructiveElevated}
-              />
-            }
-            showDivider
-          />
         </View>
       )}
     </ContextMenu>
