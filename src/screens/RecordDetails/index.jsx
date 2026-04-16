2425e8060c5ecddc0bdc20d2fb18618e0fd29383
Denis Lupookov <denislupookov@gmail.com>
2026-04-16 19:37:17 +0800

    Apply v2 FF and small additional fixes

--- src/screens/RecordDetails/index.jsx
+++ src/screens/RecordDetails/index.jsx
@@ -1,9 +1,9 @@
-import { DESIGN_VERSION } from '@tetherto/pearpass-lib-constants'
+import { MOBILE_DESIGN_VERSION } from '@tetherto/pearpass-lib-constants'
 
 import { RecordDetailsV1 } from './v1/RecordDetailsV1'
 import { RecordDetailsV2 } from './v2/RecordDetailsV2'
 
 export const RecordDetails = (props) => {
-  if (DESIGN_VERSION === 1) return <RecordDetailsV1 {...props} />
+  if (MOBILE_DESIGN_VERSION === 1) return <RecordDetailsV1 {...props} />
   return <RecordDetailsV2 {...props} />
 }
